0ae26b1c75e31b10ffe779f931dcef606fdcdf3b
TusharRawat59 <tusharrawat1302@gmail.com>
2026-01-17 03:45:02 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import UserContext from './context/userContext.jsx'
+import UserContext from './context/UserContext.jsx'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
